f1683949a46e96e2e98bce586abf20b73e3d38c0
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-11-06 12:19:03 +0300

    Ответ на задание #2

---
+++ tests.js
@@ -0,0 +1,53 @@
+let firstQuestion = prompt('1. Сколько будет 2 + 2?');
+let firstQuestionNumber =(Number(firstQuestion));
+let firstAnswer = 4;
+if (firstAnswer===firstQuestionNumber) {
+    
+    alert('Ответ Верный');
+  } else {
+4
+    alert('Ответ Неверный');	
+  }
+
+  alert(`Конец теста! Правильные ответы — ${correctAnswers}; Неправильные ответы — ${incorrectAnswers} лет `);
+
+// let secondQuestion = prompt('2. Сколько будет 2 * 2?');
+// let secondQuestionNumber = (Number(secondQuestion));
+// let secondAnswer = 4;
+// if (secondAnswer===secondQuestionNumber) {
+//     alert('Ответ Верный');
+
+//   } else {4
+//     alert('Ответ Неверный');	
+//   }
+
+// let thirdQuestion = prompt('3. У Пети было 5 яблок. 3 из них он съел, 1 отдал другу. Сколько яблок у Пети осталось?');
+// let thirdQuestionNumber = (Number(thirdQuestion));
+// let thirdAnswer = 1;
+// if (thirdAnswer===thirdQuestionNumber) {
+//     alert('Ответ Верный');
+//   } else {
+//     alert('Ответ Неверный');	
+//   }
+
+// let fourthQuestion = prompt('4. У Маши было 10 конфет. 2 она съела, 1 отдала другу. После мама дала Маше еще 5 конфет. Сколько в итоге конфет осталось у Маши?');
+// let fourthQuestionNumber = (Number(fourthQuestion));
+// let fourthAnswer = 12;
+// if (fourthAnswer===fourthQuestionNumber) {
+//     alert('Ответ Верный');
+//   } else {
+//     alert('Ответ Неверный');
+//   }
+
+// let fifthQuestion = prompt('5. Сколько будет 2 + 2 * 2?');
+// let fifthQuestionNumber = (Number(fifthQuestion));
+// let fifthAnswer = 6
+// if (fifthAnswer ===fifthQuestionNumber) {
+//     alert('Ответ Верный');
+//   } else {
+//     alert('Ответ Неверный');
+//   }
+
+
+
+
